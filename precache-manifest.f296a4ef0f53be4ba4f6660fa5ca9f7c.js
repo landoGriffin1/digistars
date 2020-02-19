4bf8ba76fbc1798e228412eea984071537e7dab9
James Allis <James.Allis@think.eu>
2020-02-19 14:39:32 +0000

    Updates

---
+++ precache-manifest.f296a4ef0f53be4ba4f6660fa5ca9f7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc8a1015269fece70ed17fed7f2e69ab",
+    "url": "/digistars/index.html"
+  },
+  {
+    "revision": "ad23eab8098c4713d0e5",
+    "url": "/digistars/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "cc994ca0858b57979c0b",
+    "url": "/digistars/static/css/main.493a4e97.chunk.css"
+  },
+  {
+    "revision": "ad23eab8098c4713d0e5",
+    "url": "/digistars/static/js/2.567be57e.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/digistars/static/js/2.567be57e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc994ca0858b57979c0b",
+    "url": "/digistars/static/js/main.9da5bc87.chunk.js"
+  },
+  {
+    "revision": "9c5a8347b9a3ebd9dab6",
+    "url": "/digistars/static/js/runtime-main.f1b7a650.js"
+  }
+]);
